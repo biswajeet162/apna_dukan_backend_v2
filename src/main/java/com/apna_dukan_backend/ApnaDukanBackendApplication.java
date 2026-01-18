b1028ae0d0035846895d5c915dbea19e360237a1
Biswajeet <biswajeet162@gmail.com>
2026-01-18 11:15:49 +0530

    init

---
+++ src/main/java/com/apna_dukan_backend/ApnaDukanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.apna_dukan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnaDukanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnaDukanBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apna_dukan_backend/ApnaDukanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apna_dukan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApnaDukanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
